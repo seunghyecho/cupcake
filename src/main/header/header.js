c776c289ebd1c64ff8f6511958e8acd110a3e2a2
seunghyecho <tmdgp523@naver.com>
2021-02-16 14:41:31 +0900

    first commit 메인,서브 layout 세팅 완료

---
+++ src/main/header/header.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    console.log('header');
+
+    var $util = $('#util');
+    var $cart = $util.children('li:first-child');
+
+    var $popup = $('.popup');
+    var $closeBtn = $popup.children().children('a');
+
+    $popup.hide();
+
+    // 팝업보이기
+    $cart.on('click', function(){
+        $('.popup').fadeIn();
+    });
+
+    // 팝업숨기기
+    $closeBtn.on('click', function(e){
+        e.preventDefault();
+        $('.popup').fadeOut();
+        
+    });
+})
